ae4660a851644eef2d01d0e34a7721e152aed92a
oooarem-hash <oooarem@gmail.com>
2026-02-12 16:35:04 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int x = 100;
+//        int y = 14;
+//        int z = 123;
+//        int sum = z + y;
+//        System.out.println(sum);
+//        System.out.println(x + y);
+//        System.out.println("Hello, world!");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
